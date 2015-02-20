25ee75ac9813aa091506fdedaf354c40f9476a0d
krainov <krainov@bpcbt.com>
2015-02-20 18:15:10 +0300

    add

--- common-services/src/test/java/ru/yesdo/service/SpatialOperationTest.java
+++ common-services/src/test/java/ru/yesdo/service/SpatialOperationTest.java
@@ -14,29 +14,31 @@ import java.util.List;
  * User: Krainov
  * Date: 20.02.2015
  * Time: 17:07
+ * http://neo4j-contrib.github.io/spatial/
  */
 public class SpatialOperationTest extends GeneralCommonServiceDbTest {
 
     @Test
     @Transactional
     public void testCreateCoordinate() {
-        SimplePointLayer layer = spatialDatabaseService.createSimplePointLayer("test-layer");
-        Coordinate coordinate1 = new Coordinate(10.0,10.0);
-        Coordinate coordinate2 = new Coordinate(20.0,20.0);
-        Coordinate coordinate3 = new Coordinate(30.0,30.0);
-
-        layer.add(coordinate1);
-        layer.add(coordinate2);
-        layer.add(coordinate3);
-
-        Coordinate myCoordinate = new Coordinate(15.0,15.0);
-
-
-        List<GeoPipeFlow> pipeFlows = layer.findClosestPointsTo(myCoordinate,0.000010);
-        for (GeoPipeFlow pipeFlow : pipeFlows) {
-            SpatialDatabaseRecord record = pipeFlow.getRecord();
-            System.out.println(record);
-        }
+        System.out.println("cleant");
+//        SimplePointLayer layer = spatialDatabaseService.createSimplePointLayer("geom");
+//        Coordinate coordinate1 = new Coordinate(10.0,10.0);
+//        Coordinate coordinate2 = new Coordinate(20.0,20.0);
+//        Coordinate coordinate3 = new Coordinate(30.0,30.0);
+//
+//        layer.add(coordinate1);
+//        layer.add(coordinate2);
+//        layer.add(coordinate3);
+//
+//        Coordinate myCoordinate = new Coordinate(15.0,15.0);
+//
+//
+//        List<GeoPipeFlow> pipeFlows = layer.findClosestPointsTo(myCoordinate,0.000010);
+//        for (GeoPipeFlow pipeFlow : pipeFlows) {
+//            SpatialDatabaseRecord record = pipeFlow.getRecord();
+//            System.out.println(record);
+//        }
     }
 
 }
